29bba2e815ce47373ebe2de032d0c98e41243f22
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2025-01-23 14:14:51 -0500

    feat: product interface and service added

---
+++ src/products/products.controller.ts
@@ -0,0 +1,49 @@
+import {
+    BadRequestException,
+    Body,
+    Controller,
+    Delete,
+    Get,
+    HttpCode,
+    HttpStatus,
+    Param,
+    Post,
+    Put,
+    Res
+} from '@nestjs/common';
+import {ProductsService} from "./products.service";
+import {Product} from "./product.interface";
+
+@Controller('products')
+export class ProductsController {
+
+    constructor(private readonly productsService: ProductsService) { }
+    @Get()
+    getAllProducts() : Product[] {
+        return this.productsService.findAll();
+    }
+
+    @Get(':id')
+    getProductById(@Param('id') id:number): Product {
+        return this.productsService.findById(id);
+    }
+
+
+
+    @Post()
+    createProduct(@Body() body: any): Product {
+        return this.productsService.addProduct(body);
+    }
+
+    @Put(':id')
+    updateProduct(@Param('id') id: number, @Body() body: any) : Product {
+        return this.productsService.updateProduct(id, body);
+    }
+
+    @Delete(':id')
+    @HttpCode(204)
+    deleteProduct(@Param('id') id: number): string {
+        this.productsService.deleteProduct(id);
+        return 'Product deleted';
+    }
+}
---
+++ src/products/products.service.ts
@@ -0,0 +1,62 @@
+import {Injectable, NotFoundException} from '@nestjs/common';
+import {Product} from "./product.interface";
+
+@Injectable()
+export class ProductsService {
+    private products: Product[] = [
+        {
+            id: 1,
+            name: 'First product',
+            description: 'This is the description of the first product',
+        },
+        {
+            id: 2,
+            name: 'Second product',
+            description: 'This is the description of the second product',
+        }
+    ];
+
+    findAll() : Product[] {
+        return this.products;
+    }
+
+    findById(id: number) : Product {
+        let product = this.products.find((product) => product.id == id);
+        if(product) return product;
+        throw new NotFoundException(`Product with id ${id} not found`);
+    }
+
+    addProduct(product: any): Product {
+        this.products = [
+            ...this.products,
+            {
+                id: this.lookUpLastId() + 1,
+                name: product.name,
+                description: product.description
+            }
+        ]
+
+        return this.findById(this.lookUpLastId());
+    }
+
+    updateProduct(id: number, product: any) : Product {
+        let updatedProduct : Product = {
+            id,
+            name: product.name,
+            description: product.description
+        }
+        this.products = this.products.map((item) => {
+            return item.id == id ? updatedProduct : item;
+        })
+
+        return this.findById(id);
+    }
+
+    deleteProduct(id: number) : void {
+        this.products = this.products.filter((item) => item.id != id);
+    }
+
+    private lookUpLastId(): number {
+        return this.products[this.products.length -1].id;
+    }
+}
