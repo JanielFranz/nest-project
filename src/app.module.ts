dc6292bae769e27c3578423dcd0f6014df88d81f
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2025-02-12 11:04:58 -0500

    feat: environment variables added

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,19 +5,30 @@ import { ProductsModule } from './products/products.module';
 import { TagsModule } from './tags/tags.module';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {Product} from "./products/entities/product.entity";
+import {ConfigModule, ConfigService} from "@nestjs/config";
 
 @Module({
-  imports: [ProductsModule, TagsModule, TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: 'password',
-    database: 'curso-nest',
-    // entities: [Product]
-    autoLoadEntities: true,
-    synchronize: true
-  })],
+  imports: [
+      ConfigModule.forRoot({
+        isGlobal: true
+      }),
+      ProductsModule,
+      TagsModule,
+      TypeOrmModule.forRootAsync({
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService) => ({
+          type: 'mysql',
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_USERNAME'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DATABASE'),
+          // entities: [Product]
+          autoLoadEntities: true,
+          synchronize: true
+        })
+      })],
   controllers: [AppController],
   providers: [AppService],
 })
